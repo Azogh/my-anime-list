6093feb31f90804359b52c88d3adc013e6aba6bc
Azogh <andre.2019021054@aluno.iffar.edu.br>
2024-08-24 10:33:19 -0300

    Initial commit with Angular project setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
 // Your web app's Firebase configuration
 export const env = {
   firebaseConfig: {
-    
+    apiKey: "AIzaSyDr_eZrlESmwwjwZNZpnkVsm-EQAQpaFKo",
+  authDomain: "myanimelist-aaaa0.firebaseapp.com",
+  projectId: "myanimelist-aaaa0",
+  storageBucket: "myanimelist-aaaa0.appspot.com",
+  messagingSenderId: "859516275165",
+  appId: "1:859516275165:web:b421184b1cace4b0600e34",
+  measurementId: "G-Y4DZG17Z11"
   },
 };
